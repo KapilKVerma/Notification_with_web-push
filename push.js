1d406b5ca360e22d2d1a21210094f37f16bdf6dc
KapilKVerma <kapilkumaarverma@gmail.com>
2021-05-17 16:40:59 +1000

    fixed

--- push.js
+++ push.js
@@ -9,5 +9,14 @@ const keys = {
 
 push.setVapidDetails("mailto:test@code.co.au", keys.publicKey, keys.privateKey);
 
-const sub = {};
+const sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/dDUNHXXoPeo:APA91bFv68HU5RkO3qB-J7EZjr57Upy9oUQzqdsSjqEkdyppFYLSQ-SkkKOXjZSE-tObkU50rZVzQONFb8INkvUhnRSTpeMwR1ueqnEr4Y1trCszGb_5tGopf0GNZYzxJVAFfM6Q6blX",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BCH7-HVEaQOKOFWFmO_vg-RxGsvRExYs_v3d17jz1PzsyiPwTbwT99vGgL5NfYwC3jd094zJViDZLO6ncM8uBEE",
+    auth: "lvR7_-LCnK38mD7phO4ciA",
+  },
+};
 push.sendNotification(sub, "test message");
